bc11d77609a67637095814161d05540ce66f6473
Sander <sandervanessel@gmail.com>
2022-04-07 13:58:07 +0200

    basic init

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div class="w-64 h-32 bg-red-500">
+        <GraphModeler />
+    </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import GraphModeler from './components/GraphModeler.vue';
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        GraphModeler
+    }
 });
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
---
+++ src/components/GraphModeler.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div class="relative h-full" id="modeler-container"></div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted } from 'vue'
+
+// const mounted = onMounted(() => {
+
+// })
+
+onMounted(() => {
+    console.log(`The initial count is something.`)
+})
+
+// return {
+
+// }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
